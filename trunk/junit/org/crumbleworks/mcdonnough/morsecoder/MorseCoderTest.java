8e6fc8e755d9867d38d4351746a8e7ee7cf17f7e
psbaechli@gmail.com <psbaechli@gmail.com@0a35d38c-f3c9-9a7c-b98f-3ba9db9a2835>
2012-04-30 21:38:23 +0000

    moved test to new folder 'junit'

---
+++ trunk/junit/org/crumbleworks/mcdonnough/morsecoder/MorseCoderTest.java
@@ -0,0 +1,40 @@
+package org.crumbleworks.mcdonnough.morsecoder;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MorseCoderTest {
+	MorseCoder morseCoder;
+
+	@Before
+	public void initializeMorseCoder() throws Exception {
+		morseCoder = new MorseCoder("./data/morsecode_general.xml");
+	}
+
+	@Test
+	public void testIfEncodeWorksAsExpected() {
+		assertEquals("...././.-../.-../---/--..--//.--/---/.-./.-../-../" + MorseCodeUtilities.ERROR_STRING + "//", morseCoder.encode("Hello, world!"));
+	}
+	
+	@Test
+	public void testIfEncodeWorksAsExpectedWithOneSpecialCasesNotSeparated() {
+		assertEquals("...././.-../.-../---/--..--//.--/---/.-./.-../-../" + MorseCodeUtilities.ERROR_STRING + "//...---...//", morseCoder.encode("Hello, world! [SOS]"));
+	}
+	
+	@Test
+	public void testIfEncodeWorksAsExpectedWithGivingAnInvalidWord() {
+		assertEquals("[undefined]/.../---/...//", morseCoder.encode("[SOS"));
+	}
+	
+	@Test
+	public void testIfEncodeWorksAsExpectedWithThreeSpecialCasesNotSeparated() {
+		assertEquals("...././.-../.-../---/--..--//.--/---/.-./.-../-../" + MorseCodeUtilities.ERROR_STRING + "//...---...//...---...//...---...//", morseCoder.encode("Hello, world! [SOS][SOS][SOS]"));
+	}
+	
+	@Test
+	public void testIfDecodeWorksAsExpected() {
+		assertEquals("HELLO, WORLD", morseCoder.decode("...././.-../.-../---/--..--/   /.--/---/.-./.-../-../"));
+	}
+}
