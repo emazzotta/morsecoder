c360478ceeb830e515138a636a06abc86a3d1dae
emanuele.mazzotta.developer@gmail.com <gta99@users.noreply.github.com>
2012-05-04 13:47:37 +0000

    Refactored PlayWave and implemented speed control (see constant "AUDIO_PLAY_SPEED")

--- src/main/java/org/crumbleworks/mcdonnough/morsecoder/PlayWave.java
+++ src/main/java/org/crumbleworks/mcdonnough/morsecoder/PlayWave.java
@@ -3,80 +3,59 @@ package org.crumbleworks.mcdonnough.morsecoder;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class PlayWave {
 
-	private String filename;
-	private Position curPosition;
-	private final int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb
-
-	enum Position {
-		LEFT, RIGHT, NORMAL
-	};
-
-	public PlayWave(String wavfile) {
-		filename = wavfile;
-		curPosition = Position.NORMAL;
+	private String wavFilePath;
+	// ENTER SPEED BETWEEN 0 - 7825
+	public static long AUDIO_PLAY_SPEED = 3200;
+	private final int EXTERNAL_BUFFER_SIZE = 1048576;
+	
+	public PlayWave(String wavFilePath) {
+		this.wavFilePath = wavFilePath;
 	}
 
 	public void play() throws FileNotFoundException {
-		File soundFile = new File(filename);
+		File soundFile = new File(wavFilePath);
+		SourceDataLine audioLine = null;
+		
 		if(!soundFile.exists()) {
-			throw new FileNotFoundException("Wave file not found: " + filename);
-		}
-
-		AudioInputStream audioInputStream = null;
-		try {
-			audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-		} catch (UnsupportedAudioFileException | IOException e) {
-			e.printStackTrace();
-			return;
+			throw new FileNotFoundException("Wave file not found: " + wavFilePath);
 		}
 		
-		AudioFormat format = audioInputStream.getFormat();
-		SourceDataLine auline = null;
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-
 		try {
-			auline = (SourceDataLine) AudioSystem.getLine(info);
-			auline.open(format);
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		if(auline.isControlSupported(FloatControl.Type.PAN)) {
-			FloatControl pan = (FloatControl) auline.getControl(FloatControl.Type.PAN);
-			if(curPosition == Position.RIGHT)
-				pan.setValue(1.0f);
-			else if(curPosition == Position.LEFT)
-				pan.setValue(-1.0f);
-		}
-
-		auline.start();
-		int nBytesRead = 0;
-		byte[] abData = new byte[EXTERNAL_BUFFER_SIZE];
-
-		try {
-			while(nBytesRead != -1) {
-				nBytesRead = audioInputStream.read(abData, 0, abData.length);
-				if(nBytesRead >= 0)
-					auline.write(abData, 0, nBytesRead);
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+			
+			AudioFormat audioFormat = audioInputStream.getFormat();
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+			
+			audioLine = (SourceDataLine) AudioSystem.getLine(info);
+			audioLine.open(audioFormat);
+			audioLine.start();
+			
+			int amountOfBytesRead = 0;
+			byte[] byteBuffer = new byte[EXTERNAL_BUFFER_SIZE];
+			
+			while(amountOfBytesRead != -1) {
+				audioInputStream.skip(AUDIO_PLAY_SPEED);
+				amountOfBytesRead = audioInputStream.read(byteBuffer, 0, byteBuffer.length);
+				if(amountOfBytesRead >= 0) {
+					audioLine.write(byteBuffer, 0, amountOfBytesRead);
+				}
 			}
-		} catch (IOException e) {
+		} catch(UnsupportedAudioFileException | IOException | LineUnavailableException e) {
 			e.printStackTrace();
-			return;
 		} finally {
-			auline.drain();
-			auline.close();
+			audioLine.drain();
+			audioLine.close();
 		}
 	}
 }
