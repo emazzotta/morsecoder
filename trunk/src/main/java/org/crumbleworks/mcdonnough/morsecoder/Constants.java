8ee65b7b640b80e59d2e364c6762ee77deb79055
emanuele.mazzotta.developer@gmail.com <emanuele.mazzotta.developer@gmail.com@0a35d38c-f3c9-9a7c-b98f-3ba9db9a2835>
2012-05-05 02:24:29 +0000

    Fixed some issues in main class One issue left: loading sources in order to use them properly with .jar

--- trunk/src/main/java/org/crumbleworks/mcdonnough/morsecoder/Constants.java
+++ trunk/src/main/java/org/crumbleworks/mcdonnough/morsecoder/Constants.java
@@ -1,5 +1,7 @@
 package org.crumbleworks.mcdonnough.morsecoder;
 
+import java.net.URL;
+
 public class Constants {
 
 	public static int AUDIO_EXTERNAL_BUFFER_SIZE = 1048576;
@@ -9,10 +11,15 @@ public class Constants {
 	public static final long AUDIO_PLAY_VERY_FAST_SPEED = 7000;
 	public static long AUDIO_MORSECODE_SPACE_SLEEP = 250;
 	
-	public static final String RESOURCE_PATH = "src/main/resources/";
-	public static final String AUDIO_WAV_MORSECODE_SHORT_PATH = Constants.RESOURCE_PATH + "sfx/short.wav";
-	public static final String AUDIO_WAV_MORSECODE_LONG_PATH = Constants.RESOURCE_PATH + "sfx/long.wav";
-	public static final String MORSECODE_CONTENT_XML_PATH = Constants.RESOURCE_PATH + "xml/morsecode.xml";
+	public static final String AUDIO_WAV_MORSECODE_SHORT_PATH = createPath(Constants.class.getClassLoader().getResource("sfx/short.wav"));
+	public static final String AUDIO_WAV_MORSECODE_LONG_PATH = createPath(Constants.class.getClassLoader().getResource("sfx/long.wav"));
+	public static final String MORSECODE_CONTENT_XML_PATH = createPath(Constants.class.getClassLoader().getResource("xml/morsecode.xml"));
 	
 	public static final String ERROR_STRING = "[undefined]";
+	
+	private static String createPath(URL filePath) {
+		String url = String.valueOf(filePath);
+		int startPos = url.indexOf("C:");
+		return url.substring(startPos);
+	}
 }
