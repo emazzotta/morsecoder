4f19bf91da222b23a9424597396cb857d982b7e5
emanuele.mazzotta.developer@gmail.com <gta99@users.noreply.github.com>
2012-05-05 02:24:29 +0000

    Fixed some issues in main class One issue left: loading sources in order to use them properly with .jar

--- src/main/java/org/crumbleworks/mcdonnough/morsecoder/Main.java
+++ src/main/java/org/crumbleworks/mcdonnough/morsecoder/Main.java
@@ -8,46 +8,55 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 public class Main {
 
 	public static void main(String args[]) throws InvalidMorseCodeAudioOutputException, InterruptedException, UnsupportedAudioFileException, IOException, LineUnavailableException {
-		Encoder encoder = new Encoder();
-		Decoder decoder = new Decoder();
+		Encoder encoder;
+		Decoder decoder;
 		MorseCodePlayer player;
-		String text = args[args.length-1];
-		
-		for(int i=0;i<args.length-1;i++) {
-			if(args[i].equals("-e") || args[i].equals("--encode")) {
-				System.out.println("Encoding '" + text + "'...");
-				System.out.println("Encoded text in morsecode: " + encoder.encode(text));
-			} else if(args[i].equals("-d") || args[i].equals("--decode")) {
-				System.out.println("Decoding '" + text + "'...");
-				System.out.println("Decoded morsecode in text: " + decoder.decode(text));
-			} else if(args[i].equals("-p") || args[i].equals("--play")) {
-				System.out.println("Playing morsecode...");
-				player = new MorseCodePlayer(text);
-				if(player.isMorseCode()) {
+
+		if(args.length == 0) {
+			System.out.println(new Main().getInvalidArgumentMessage(""));
+		} else if(args[0].equals("--help")) {
+			System.out.println(new Main().getUsageHelp());
+		} else if(args.length == 1) {
+			System.out.println(new Main().getInvalidArgumentMessage(args[0]));
+		} else {
+			String text = args[args.length-1];
+			for(int i=0;i<args.length-1;i++) {
+				if(args[i].equals("-e") || args[i].equals("--encode")) {
+					encoder = new Encoder();
+					System.out.println("Encoding '" + text + "'...");
+					System.out.println("Encoded text in morsecode: " + encoder.encode(text));
+				} else if(args[i].equals("-d") || args[i].equals("--decode")) {
+					decoder = new Decoder();
+					System.out.println("Decoding '" + text + "'...");
+					System.out.println("Decoded morsecode in text: " + decoder.decode(text));
+				} else if(args[i].equals("-p") || args[i].equals("--play")) {
+					encoder = new Encoder();
+					System.out.println("Playing morsecode...");
+					player = new MorseCodePlayer(text);
+					if(!player.isMorseCode()) {
+						player = new MorseCodePlayer(encoder.encode(text));
+					}
 					player.play();
 				} else {
-					player = new MorseCodePlayer(encoder.encode(text));
+					System.out.println(new Main().getInvalidArgumentMessage(args[i]));
+					break;
 				}
-			} else if(args[i].equals("--help")) {
-				new Main().getUsageHelp();
-			} else {
-				new Main().getInvalidArgumentMessage(args[i]);
 			}
 		}
 	}
 	
 	private String getUsageHelp() {
-		String usageHelp = "CrumbleWorks MorseCoder by Patrick Baechli & Emanuele Mazzotta ver. 1.0 - A Morsecode Library.\n";
+		String usageHelp = "\nCrumbleWorks MorseCoder by Patrick Baechli & Emanuele Mazzotta ver. 1.0 - A Morsecode Library.\n";
 		usageHelp += "Usage: java -jar MorseCoder.jar [OPTION] [TEXT]\n\n";
 		
 		usageHelp += "At least one argument mandatory.\n\n";
 		
 		usageHelp += "Convert:\n";
-		usageHelp += "-e, --encode\t\tEncode the text to morsecode.\n";
-		usageHelp += "-d, --decode\t\tDecode the morsecode to text.\n";
+		usageHelp += "-e, --encode\tEncode the text to morsecode.\n";
+		usageHelp += "-d, --decode\tDecode the morsecode to text.\n";
 		
 		usageHelp += "Play:\n";
-		usageHelp += "-p, --play\t\tAudio output morsecode. Can be text or morsecode.\n";
+		usageHelp += "-p, --play\tAudio output morsecode. Can be text or morsecode.\n";
 		
 		return usageHelp;
 	}
