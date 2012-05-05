23a6b789c9e33fba51288b862a64095aad48a015
emanuele.mazzotta.developer@gmail.com <emanuele.mazzotta.developer@gmail.com@0a35d38c-f3c9-9a7c-b98f-3ba9db9a2835>
2012-05-05 23:37:37 +0000

    Modified output name of .jar, some changes in main done

--- trunk/src/main/java/org/crumbleworks/mcdonnough/morsecoder/MorseCodePlayer.java
+++ trunk/src/main/java/org/crumbleworks/mcdonnough/morsecoder/MorseCodePlayer.java
@@ -7,20 +7,23 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class MorseCodePlayer {
 
+    	private long audioMorseCodeSpaceSleep = Constants.AUDIO_MORSECODE_SPACE_SLEEP;
+    	private long audioPlaySpeed = Constants.AUDIO_PLAY_MODERATE_SPEED;
+
 	private WavePlayer playShortWave;
 	private WavePlayer playLongWave;
 	private String morseCode;
 	
 	public MorseCodePlayer(String morseCode) {
 		this.morseCode = morseCode;
-		playShortWave = new WavePlayer(Constants.AUDIO_WAV_MORSECODE_SHORT_PATH);
-		playLongWave = new WavePlayer(Constants.AUDIO_WAV_MORSECODE_LONG_PATH);
+		playShortWave = new WavePlayer(Constants.AUDIO_WAV_MORSECODE_SHORT_PATH, audioMorseCodeSpaceSleep);
+		playLongWave = new WavePlayer(Constants.AUDIO_WAV_MORSECODE_LONG_PATH, audioPlaySpeed);
 	}
 	
-	public boolean isMorseCode() throws InvalidMorseCodeAudioOutputException {
+	public boolean isMorseCode() {
 		for(int i=0;i<morseCode.length();i++) {
 			if(morseCode.charAt(i) != '.' && morseCode.charAt(i) != '-' && morseCode.charAt(i) != '/') {
-				throw new InvalidMorseCodeAudioOutputException("String to Audio-Output Contains Invalid Character(s)");
+			    return false;
 			}
 		}
 		return true;
@@ -34,9 +37,19 @@ public class MorseCodePlayer {
 				playShortWave.play();
 			} else if(morseCode.charAt(i) == '-') {
 				playLongWave.play();
+			} else if(morseCode.charAt(i) == '/') {
+				Thread.sleep(audioMorseCodeSpaceSleep);
 			} else {
-				Thread.sleep(Constants.AUDIO_MORSECODE_SPACE_SLEEP);
+			    throw new InvalidMorseCodeAudioOutputException("String to Audio-Output Contains Invalid Character(s)");
 			}
 		}
+	}
+	
+	public void setAudioMorseCodeSpaceSleep(long audioMorseCodeSpaceSleep) {
+	    this.audioMorseCodeSpaceSleep = audioMorseCodeSpaceSleep;
+	}
+
+	public void setAudioPlaySpeed(long audioPlaySpeed) {
+	    this.audioPlaySpeed = audioPlaySpeed;
 	}
 }
--- trunk/src/main/java/org/crumbleworks/mcdonnough/morsecoder/WavePlayer.java
+++ trunk/src/main/java/org/crumbleworks/mcdonnough/morsecoder/WavePlayer.java
@@ -14,9 +14,11 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 public class WavePlayer {
 
 	private String wavFilePath;
+	private long audioPlaySpeed;
 	
-	public WavePlayer(String wavFilePath) {
+	public WavePlayer(String wavFilePath, long audioPlaySpeed) {
 		this.wavFilePath = wavFilePath;
+		this.audioPlaySpeed = audioPlaySpeed;
 	}
 
 	public void play() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
@@ -34,7 +36,7 @@ public class WavePlayer {
 		byte[] byteBuffer = new byte[Constants.AUDIO_EXTERNAL_BUFFER_SIZE];
 		
 		while(amountOfBytesRead != -1) {
-			audioInputStream.skip(Constants.AUDIO_PLAY_MODERATE_SPEED);
+			audioInputStream.skip(audioPlaySpeed);
 			amountOfBytesRead = audioInputStream.read(byteBuffer, 0, byteBuffer.length);
 			if(amountOfBytesRead >= 0) {
 				audioLine.write(byteBuffer, 0, amountOfBytesRead);
