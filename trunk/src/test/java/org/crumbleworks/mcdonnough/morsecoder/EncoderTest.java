962f45ec938e7ea5bd5dbcf998bfd5efb8756ca3
psbaechli@gmail.com <psbaechli@gmail.com@0a35d38c-f3c9-9a7c-b98f-3ba9db9a2835>
2012-05-06 14:21:01 +0000

    formatted source files

--- trunk/src/test/java/org/crumbleworks/mcdonnough/morsecoder/EncoderTest.java
+++ trunk/src/test/java/org/crumbleworks/mcdonnough/morsecoder/EncoderTest.java
@@ -5,31 +5,32 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class EncoderTest {
-	
-	Encoder morseEncoder = new Encoder();
-	
-	@Test
-	public void testIfEncodeWorksAsExpected() {
-		assertEquals("...././.-../.-../---/--..--//.--/---/.-./.-../-../" + Constants.ERROR_STRING + "//", morseEncoder.encode("Hello, world!"));
-	}
-	
-	@Test
-	public void testIfEncodeWorksAsExpectedWithOneSpecialCasesNotSeparated() {
-		assertEquals("...././.-../.-../---/--..--//.--/---/.-./.-../-../" + Constants.ERROR_STRING + "//...---...//", morseEncoder.encode("Hello, world! [SOS]"));
-	}
-	
-	@Test
-	public void testIfEncodeWorksAsExpectedWithGivingAnInvalidWord() {
-		assertEquals("[undefined]/.../---/...//", morseEncoder.encode("[SOS"));
-	}
-	
-	@Test
-	public void testIfEncodeWorksAsExpectedWithThreeSpecialCasesNotSeparated() throws InvalidMorseCodeAudioOutputException {
-		assertEquals("...././.-../.-../---/--..--//.--/---/.-./.-../-../" + Constants.ERROR_STRING + "//...---...//...---...//...---...//", morseEncoder.encode("Hello, world! [SOS][SOS][SOS]"));
-	}
-	
-	@Test
-	public void testIfExpectedAmountOfOccurencesIsFound() {
-		assertEquals(3, morseEncoder.findOccurencesOfSequenceInString("o", "oozoa"));
-	}
+
+    Encoder morseEncoder = new Encoder();
+
+    @Test
+    public void testIfEncodeWorksAsExpected() {
+        assertEquals("...././.-../.-../---/--..--//.--/---/.-./.-../-../" + Constants.ERROR_STRING + "//", morseEncoder.encode("Hello, world!"));
+    }
+
+    @Test
+    public void testIfEncodeWorksAsExpectedWithOneSpecialCasesNotSeparated() {
+        assertEquals("...././.-../.-../---/--..--//.--/---/.-./.-../-../" + Constants.ERROR_STRING + "//...---...//", morseEncoder.encode("Hello, world! [SOS]"));
+    }
+
+    @Test
+    public void testIfEncodeWorksAsExpectedWithGivingAnInvalidWord() {
+        assertEquals("[undefined]/.../---/...//", morseEncoder.encode("[SOS"));
+    }
+
+    @Test
+    public void testIfEncodeWorksAsExpectedWithThreeSpecialCasesNotSeparated() throws InvalidMorseCodeAudioOutputException {
+        assertEquals("...././.-../.-../---/--..--//.--/---/.-./.-../-../" + Constants.ERROR_STRING + "//...---...//...---...//...---...//",
+                morseEncoder.encode("Hello, world! [SOS][SOS][SOS]"));
+    }
+
+    @Test
+    public void testIfExpectedAmountOfOccurencesIsFound() {
+        assertEquals(3, morseEncoder.findOccurencesOfSequenceInString("o", "oozoa"));
+    }
 }
